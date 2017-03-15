7f65cbbfd285fb3104eb94936bba7f717c965b36
rkhunter <rkhunter@xxi-empire.ru>
2017-03-15 21:31:53 +0200

    Public version

--- src/main/java/BatchEmailSender.java
+++ src/main/java/BatchEmailSender.java
@@ -421,7 +421,7 @@ public class BatchEmailSender {
 
         MimeMessage email = new MimeMessage(session);
 
-        email.setFrom(new InternetAddress("sergiykat@gmail.com"));
+        email.setFrom(new InternetAddress("youremail@gmail.com"));
         email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
         email.setSubject(subject);
 
